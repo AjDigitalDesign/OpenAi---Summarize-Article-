c3035117193bbdfd00cdf8ed56b2cb13d3c5cd8a
ajdigitaldesign <ajardiahjr@gmail.com>
2024-02-12 21:37:28 -0600

    updated vercel.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   define: {
+//     __APP_ENV__: JSON.stringify(env.APP_ENV),
+//   },
+//   plugins: [react()],
+// });
+
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_RAPID_API_ARTICLE_KEY,
+    },
+    plugins: [react()],
+  };
+});
